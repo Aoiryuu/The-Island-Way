5f219e48ec68a4b75d8bde97466378807be6ef3d
アキヨシ <honowai.lmoniz01@gmail.com>
2018-04-16 19:30:00 -1000

    changed the image and made functions

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-var Points = {
-	cities  = [],
-	landmarks = []
-};
+ var points = [
+ 	{id:1 ,
+ 		city: "Waipahu"},
+ 	{id:2,
+ 		city:"PearlBity" }
+]
+console.log(points)
+var currentCity = null;
+function showData(city){
+	//document.getElementById(currentCity).style.display = "none"
+	document.getElementById(city).style.display="block"
+	currentCity = city;
+}
+function hideCity(currentCity){
+	document.getElementById(currentCity).style.display = "none"
+}
